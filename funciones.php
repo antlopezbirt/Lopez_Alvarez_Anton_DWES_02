4d14375c3412a0748cba68c9c06f9e1524119cbe
Antón López <antlopez@birt.eus>
2024-11-05 13:55:28 +0100

    Creación validarFormulario y aislamiento en funciones.php

--- funciones.php
+++ funciones.php
@@ -2,6 +2,7 @@
 
 declare(strict_types = 1);
 
+// Valida un DNI usando el algoritmo de módulo 23
 function validarDNI(string $dni) {
     $numero = (int) substr($dni, 0, 8);
     $letra = substr($dni, -1, 1);
@@ -13,6 +14,7 @@ function validarDNI(string $dni) {
     return false;
 }
 
+// Valida un usuario comprobando que exista en la estructura de datos
 function validarUsuario(string $nombre, string $apellidos, string $dni) {
     foreach (USUARIOS as $usuario) {
         if ($usuario["nombre"] === $nombre && 
@@ -24,15 +26,18 @@ function validarUsuario(string $nombre, string $apellidos, string $dni) {
     return false;
 }
 
+// Valida una fecha de inicio, comprobando que sea mayor que la actual
 function validarFecha(string $fecha) {
     return (strtotime($fecha) > time());
 }
 
+// Valida una duración, comprobando que se encuentre en el rango requerido
 function validarDuracion(int $duracion) {
     return ($duracion >= 1 && $duracion <= 30);
 }
 
-function reservarModelo(int $modelo) {
+// Valida la disponibilidad de un modelo, comprobando los datos
+function validarModelo(int $modelo) {
     global $coches;
     foreach ($coches as $coche) {
         if ($coche["id"] === $modelo && $coche["disponible"] === true)
@@ -40,4 +45,76 @@ function reservarModelo(int $modelo) {
     }
 
     return false;
+}
+
+// Coordina la validación de todos los datos enviados desde el formulario
+function validarFormulario() {
+
+    // Este boolean controla si alguna validación ha fallado
+    $validado = true;
+    // La cadena va acumulando cada mensaje de error para mostrarlos al finalizar
+    $mensajes = '';
+
+    // Comprobación nombre vacío
+    if (trim($_POST['nombre']) === '') {
+        $mensajes .= "<span style='background: red;'>Nombre está vacío.<br>";
+        $validado = false;
+    } else {
+        $mensajes .= "<span style='background: green;'>Nombre: {$_POST['nombre']}.<br>";
+    }
+    
+    // Comprobación apellidos vacíos
+    if (trim($_POST['apellidos']) === '') {
+        $mensajes .= "<span style='background: red;'>Apellidos está vacío<br>";
+        $validado = false;
+    } else {
+        $mensajes .= "<span style='background: green;'>Apellidos: {$_POST['apellidos']}.<br>";
+    }
+    
+    // Valida DNI
+    if (!validarDNI($_POST['dni'])) {
+        $mensajes .= "<span style='background: red;'>DNI no válido: {$_POST['dni']}<br>";
+        $validado = false;
+    } else {
+        $mensajes .= "<span style='background: green;'>DNI: {$_POST['dni']}.<br>";
+    }
+    
+    // Valida usuario
+    if (!validarUsuario($_POST['nombre'], $_POST['apellidos'], $_POST['dni'])) {
+        $mensajes .= "<span style='background: red;'>El usuario no es válido<br>";
+        $validado = false;
+    } else {
+        $mensajes .= "<span style='background: green;'>El usuario es válido.<br>";
+    }
+    
+    // Valida fecha
+    if (!validarFecha($_POST['fecha'])) {
+        $mensajes .= "<span style='background: red;'>La fecha ({$_POST['fecha']}) debe ser posterior a la actual<br>";
+        $validado = false;
+    } else {
+        $mensajes .= "<span style='background: green;'>La fecha es válida.<br>";
+    }
+    
+    // Valida duración
+    if (!validarDuracion((int) $_POST['duracion'])) {
+        $mensajes .= "<span style='background: red;'>La duración debe ser un número entero entre 1 y 30 (ambos incluidos).<br>";
+        $validado = false;
+    } else {
+        $mensajes .= "<span style='background: green;'>Duración: {$_POST['duracion']} días.<br>";
+    }
+    
+    // Valida disponibilidad modelo
+    if (!validarModelo((int) $_POST['modelo'])) {
+        $mensajes .= "<span style='background: red;'>Ese modelo no está disponible.<br>";
+        $validado = false;
+    } else {
+        $mensajes .= "<span style='background: green;'>El modelo está disponible.<br>";
+    }
+    
+    // Si todo ha ido bien, devuelve el mensaje de éxito, en caso contrario los mensajes
+    if ($validado) {
+        return "<h1>" . $_POST['nombre'] . " " . $_POST['apellidos'] . "</h1><br><img src='img/{$_POST['modelo']}.jpg'>";
+    } else {
+        return $mensajes;
+    }
 }
--- validacion.php
+++ validacion.php
@@ -12,65 +12,11 @@
 require_once('datos.php');
 include_once('funciones.php');
 
-//var_dump($_POST);
+//var_dump($_POST); // Para testear
 
-$validado = true;
-$cadenaErrores = '';
-
-if (trim($_POST['nombre']) === '') {
-    $cadenaErrores .= "<span style='background: red;'>Nombre está vacío.<br>";
-    $validado = false;
-} else {
-    $cadenaErrores .= "<span style='background: green;'>Nombre: {$_POST['nombre']}.<br>";
-}
-
-if (trim($_POST['apellidos']) === '') {
-    $cadenaErrores .= "<span style='background: red;'>Apellidos está vacío<br>";
-    $validado = false;
-} else {
-    $cadenaErrores .= "<span style='background: green;'>Apellidos: {$_POST['apellidos']}.<br>";
-}
-
-if (!validarDNI($_POST['dni'])) {
-    $cadenaErrores .= "<span style='background: red;'>DNI no válido: {$_POST['dni']}<br>";
-    $validado = false;
-} else {
-    $cadenaErrores .= "<span style='background: green;'>DNI: {$_POST['dni']}.<br>";
-}
-
-if (!validarUsuario($_POST['nombre'], $_POST['apellidos'], $_POST['dni'])) {
-    $cadenaErrores .= "<span style='background: red;'>El usuario no es válido<br>";
-    $validado = false;
-} else {
-    $cadenaErrores .= "<span style='background: green;'>El usuario es válido.<br>";
-}
-
-if (!validarFecha($_POST['fecha'])) {
-    $cadenaErrores .= "<span style='background: red;'>La fecha ({$_POST['fecha']}) debe ser posterior a la actual<br>";
-    $validado = false;
-} else {
-    $cadenaErrores .= "<span style='background: green;'>La fecha es válida.<br>";
-}
-
-if (!validarDuracion((int) $_POST['duracion'])) {
-    $cadenaErrores .= "<span style='background: red;'>La duración debe ser un número entero entre 1 y 30 (ambos incluidos).<br>";
-    $validado = false;
-} else {
-    $cadenaErrores .= "<span style='background: green;'>Duración: {$_POST['duracion']} días.<br>";
-}
-
-if (!reservarModelo((int) $_POST['modelo'])) {
-    $cadenaErrores .= "<span style='background: red;'>Ese modelo no está disponible.<br>";
-    $validado = false;
-} else {
-    $cadenaErrores .= "<span style='background: green;'>El modelo está disponible.<br>";
-}
-
-if ($validado) {
-    echo "<h1>" . $_POST['nombre'] . " " . $_POST['apellidos'] . "</h1><br><img src='img/{$_POST['modelo']}.jpg'>";
-} else {
-    echo $cadenaErrores;
-}
+// Si se reciben datos los valida, en caso contrario saca un aviso.
+if (count($_POST) > 0) echo validarFormulario();
+else echo "No se recibieron datos del <a href='index.php'>formulario</a>";
 
 ?>
 
